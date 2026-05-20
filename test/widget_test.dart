bf7ffe8011f1199f4955fb64761b0bd286d44e1f
Daniel Bara Seftino <5026231169@student.its.ac.id>
2026-05-20 22:37:08 +0700

    fix: remove unnecessary blank line in widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@ import 'package:sikulak/welcome_page.dart';
 void main() {
   testWidgets('app launches with WelcomePage', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
-    
+
     // Let async initialization complete
     for (int i = 0; i < 5; i++) {
       await tester.pump(const Duration(seconds: 2));
